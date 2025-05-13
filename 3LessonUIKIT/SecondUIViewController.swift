06a8c69ff1a3a32335a07bbde88fd083732cf929
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-05-13 12:43:40 +0200

    Here is first Page with BeginUIKIT

---
+++ 3LessonUIKIT/SecondUIViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondUIViewController.swift
+//  3LessonUIKIT
+//
+//  Created by Авазбек Надырбек уулу on 13.05.25.
+//
+
+import UIKit
+
+class SecondUIViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
